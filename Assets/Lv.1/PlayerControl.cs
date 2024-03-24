c4a7cd789bd83407698348f4cdb1d415c8c14001
TanaritDoungsub <tanarit.doun@bumail.net>
2024-03-24 17:41:08 +0700

    Test Score 2

--- Assets/Lv.1/PlayerControl.cs
+++ Assets/Lv.1/PlayerControl.cs
@@ -30,5 +30,10 @@ public class PlayerControl : MonoBehaviour
         {
             other.gameObject.SetActive(false);
         }
+
+        if (other.gameObject.tag == "Item") 
+        {
+            ScoreManager.scoreCount += 1;
+        }
     }
 }
