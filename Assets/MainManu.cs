f4ca84759f72b47e94f662de4f43896d629f25fc
TanaritDoungsub <tanarit.doun@bumail.net>
2024-03-25 18:29:20 +0700

    Main Manu Button + Modify BG , Score

---
+++ Assets/MainManu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainManu : MonoBehaviour
+{
+    public void Home()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
+
+
